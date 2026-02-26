6230b82c7c60aaa1b1a4fc336f1bac93fe721283
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2026-02-26 09:28:34 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module ph.com.guanzongroup.tcs {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ph.com.guanzongroup.tcs to javafx.fxml;
+    exports ph.com.guanzongroup.tcs;
+}
+*/
